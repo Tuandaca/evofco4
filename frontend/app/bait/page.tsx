4db19a6aa6504761646adeb89dad66932ef3c3fd
Tuandaca <ngobongsu23@gmail.com>
2026-09-03 01:55:13 +0700

    style: fix slider thumb shape to be circle

--- frontend/app/bait/page.tsx
+++ frontend/app/bait/page.tsx
@@ -554,8 +554,9 @@ export default function BaitAnalysisPage() {
                     step="0.1"
                     value={targetBars}
                     onChange={(e) => setTargetBars(parseFloat(e.target.value))}
-                    className="w-full h-1.5 bg-zinc-800 rounded-lg appearance-none cursor-pointer accent-blue-500 relative z-10"
+                    className="w-full h-1.5 bg-zinc-800 rounded-lg appearance-none cursor-pointer relative z-10 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-blue-500 [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-blue-500 [&::-moz-range-thumb]:border-0"
                   />
+
                   
                   {/* Labels */}
                   <div className="absolute top-8 left-0 right-0 pointer-events-none px-[8px]">
